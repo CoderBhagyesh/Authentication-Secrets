92fd2a35922894bfa71b62b8f41b6c0ed831de26
unknown <bhagyeshgk143@gmail.com>
2023-03-20 11:01:30 +0530

    Security updated to level-3 using bycrypt

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//jshint esversion:6
+
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -17,8 +18,6 @@ userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
@@ -34,12 +33,20 @@ app.get('/login', function(req,res) {
   res.render('login');
 });
 
+app.get('/logout', function(req, res) {
+  res.render('home');
+})
+
 app.post('/register', function(req,res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-  newUser.save().then(res.render('secrets'));
+  const myPlaintextPassword = req.body.password;
+  bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+      newUser.save().then(res.render('secrets'));
+  });
+
 });
 
 app.post('/login', async function(req,res) {
@@ -47,14 +54,14 @@ app.post('/login', async function(req,res) {
   const password = req.body.password;
 
     const doc = await User.findOne({email: username}).exec();
-    if(doc.email === username && doc.password===password) {
-      console.log(doc.password);
-      res.render('secrets');
-    }
-    else {
-      res.send("Incorrect username or password");
-    }
-})
+    bcrypt.compare(password, doc.password, function(err, result) {
+      if(result===true) {
+        res.render('secrets');
+      } else {
+        res.send("Your password is incorrect");
+      }
+    });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
